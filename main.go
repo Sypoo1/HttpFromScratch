ea1aee2e3b075959667b7de84fc7c98d94c5964d
Rustam Shamsutdinov <sypooit@gmail.com>
2025-11-23 05:04:33 +0300

    read from goroutine

--- main.go
+++ main.go
@@ -17,33 +17,44 @@ func main() {
 		log.Fatal("could not open %s: %s\n", filePath, err)
 	}
 
-	defer f.Close()
+	linesChan := getLinesChannel(f)
+	for line := range linesChan {
+		fmt.Printf("read: %s\n", line)
+	}
+}
 
-	fmt.Printf("Reading data from %s\n", filePath)
 
-	currentLineContents := ""
-	for{
-		buffer := make([]byte, 8, 8)
-		n, err := f.Read(buffer)
-		if err != nil{
-			if currentLineContents != "" {
-				fmt.Printf("read: %s\n", currentLineContents)
-				currentLineContents = ""
-			}
-			if errors.Is(err, io.EOF){
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(lines)
+		currentLineContents := ""
+		for{
+			buffer := make([]byte, 8, 8)
+			n, err := f.Read(buffer)
+			if err != nil{
+				if currentLineContents != "" {
+					lines <- currentLineContents
+					currentLineContents = ""
+				}
+				if errors.Is(err, io.EOF){
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
 				break
 			}
-			fmt.Printf("error: %s\n", err.Error())
-			break
-		}
-		str := string(buffer[:n])
-		parts := strings.Split(str, "\n")
-		for i := 0; i < len(parts) - 1; i++ {
-			currentLineContents += parts[i]
-			fmt.Printf("read: %s\n", currentLineContents)
-			currentLineContents = ""
-		}
-		currentLineContents += parts[len(parts) - 1]
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts) - 1; i++ {
+				currentLineContents += parts[i]
+				lines <- currentLineContents
+				currentLineContents = ""
+			}
+			currentLineContents += parts[len(parts) - 1]
 
-	}
+		}
+	}()
+	return lines
 }
