e5f469b696d118ec00b698d80c197bd530b856c5
Sypoo <sypooit@gmail.com>
2025-12-01 14:58:11 +0300

    srart parser

--- internal/requests/request.go
+++ internal/requests/request.go
@@ -1,1 +1,23 @@
 package requests
+
+import (
+	"io"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion string
+	RequestTarget string
+	Method string
+}
+
+
+func RequestFromReader(reader io.Reader) (*Request, error){
+
+	rl := RequestLine{"1.1", "/", "GET"}
+	r := Request{rl}
+	return &r, nil
+}
