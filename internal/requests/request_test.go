6f54ee517db1d780c9886a772b6d7947965cd6ba
Sypoo <sypooit@gmail.com>
2025-12-01 10:48:58 +0300

    add tests

---
+++ internal/requests/request_test.go
@@ -0,0 +1,12 @@
+package requests
+
+
+import (
+	"testing"
+
+	"github.com/stretchr/testifyl/assert"
+)
+
+func TestRequestLineRarse(t *testing.T) {
+	assert.Equal(t, "TheTestagen", "TheTestagen")
+}
