3420be2c112fa43970f0ebd40208b3716c7485c0
Rustam Shamsutdinov <sypooit@gmail.com>
2025-12-02 13:26:40 +0300

    parse start line

--- internal/requests/request.go
+++ internal/requests/request.go
@@ -1,7 +1,10 @@
 package requests
 
 import (
+	"errors"
 	"io"
+	"slices"
+	"strings"
 )
 
 type Request struct {
@@ -9,15 +12,70 @@ type Request struct {
 }
 
 type RequestLine struct {
-	HttpVersion string
+	HttpVersion   string
 	RequestTarget string
-	Method string
+	Method        string
 }
 
+var ErrReadingMessage = errors.New("can not read the message")
+var ErrParsingMessage = errors.New("can not parse the message")
+var ErrHttpVersion = errors.New("unsupported http protocol version")
+var ErrMethod = errors.New("unsupported method")
 
-func RequestFromReader(reader io.Reader) (*Request, error){
+const SEPARATOR = "\r\n"
 
-	rl := RequestLine{"1.1", "/", "GET"}
-	r := Request{rl}
-	return &r, nil
+var methods = []string{
+	"GET",
+	"HEAD",
+	"POST",
+	"PUT",
+	"DELETE",
+	"CONNECT",
+	"OPTIONS",
+	"TRACE",
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, ErrReadingMessage
+	}
+	str := string(data)
+	requestLine, _, err := parseRequestLine(str)
+	if err != nil {
+		return nil, err
+	}
+	request := Request{*requestLine}
+	return &request, nil
+}
+
+func parseRequestLine(data string) (*RequestLine, string, error) {
+	idx := strings.Index(data, SEPARATOR)
+	if idx == -1 {
+		return nil, "", ErrParsingMessage
+	}
+	requestLine := data[:idx]
+	restOfMessage := data[idx+len(SEPARATOR):]
+
+	parts := strings.Split(requestLine, " ")
+	if len(parts) != 3 {
+		return nil, restOfMessage, ErrParsingMessage
+	}
+
+	method := parts[0]
+	requestTarget := parts[1]
+	protocol := parts[2]
+
+	if !slices.Contains(methods, method) {
+		return nil, restOfMessage, ErrMethod
+	}
+
+	if protocol != "HTTP/1.1" {
+		return nil, restOfMessage, ErrHttpVersion
+	}
+
+	rl := RequestLine{"1.1", requestTarget, method}
+
+	return &rl, restOfMessage, nil
+
 }
--- internal/requests/request_test.go
+++ internal/requests/request_test.go
@@ -28,4 +28,8 @@ func TestRequestLineRarse(t *testing.T) {
 	// Test: Invalid number of parts in request line
 	_, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.Error(t, err)
+
+	// Test: Http version
+	_, err = RequestFromReader(strings.NewReader("GET / HTTP/1.0\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
 }
