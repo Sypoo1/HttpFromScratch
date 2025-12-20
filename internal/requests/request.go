1452af76a5e67706abd6d053469720bc71df5706
Rustam Shamsutdinov <sypooit@gmail.com>
2025-12-20 22:03:33 +0300

    done stream parsing

--- internal/requests/request.go
+++ internal/requests/request.go
@@ -1,14 +1,22 @@
 package requests
 
 import (
+	"bytes"
 	"errors"
 	"io"
-	"slices"
-	"strings"
+)
+
+type parserState string
+
+const (
+	StateInit  parserState = "init"
+	StateDone  parserState = "done"
+	StateError parserState = "error"
 )
 
 type Request struct {
 	RequestLine RequestLine
+	state       parserState
 }
 
 type RequestLine struct {
@@ -20,64 +28,131 @@ type RequestLine struct {
 var ErrReadingMessage = errors.New("can not read the message")
 var ErrParsingMessage = errors.New("can not parse the message")
 var ErrHttpVersion = errors.New("unsupported http protocol version")
+var ErrRequestInErrorState = errors.New("request in error state")
 var ErrMethod = errors.New("unsupported method")
 
-const SEPARATOR = "\r\n"
+var SEPARATOR = []byte("\r\n")
 
-var methods = []string{
-	"GET",
-	"HEAD",
-	"POST",
-	"PUT",
-	"DELETE",
-	"CONNECT",
-	"OPTIONS",
-	"TRACE",
+var methods = [][]byte{
+	[]byte("GET"),
+	[]byte("HEAD"),
+	[]byte("POST"),
+	[]byte("PUT"),
+	[]byte("PATCH"),
+	[]byte("DELETE"),
+	[]byte("CONNECT"),
+	[]byte("OPTIONS"),
+	[]byte("TRACE"),
+}
+
+func newRequest() *Request {
+	return &Request{
+		state: StateInit,
+	}
+}
+func (r *Request) parse(data []byte) (int, error) {
+	read := 0
+outer:
+	for {
+		switch r.state {
+		case StateInit:
+			rl, n, err := parseRequestLine(data[read:])
+			if err != nil {
+				r.state = StateError
+				return 0, err
+			}
+
+			if n == 0 {
+				break outer
+			}
+
+			read += n
+			r.RequestLine = *rl
+
+			r.state = StateDone
+
+		case StateDone:
+			break outer
+		case StateError:
+			return 0, ErrRequestInErrorState
+		}
+
+	}
+
+	return read, nil
+
+}
+
+func (r *Request) done() bool {
+	return r.state == StateDone || r.state == StateError
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	data, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, ErrReadingMessage
+	request := newRequest()
+	buf := make([]byte, 1024)
+	bufLen := 0
+	for !request.done() {
+		n, err := reader.Read(buf[bufLen:])
+		if err != nil {
+			return nil, err
+		}
+
+		bufLen += n
+
+		readN, err := request.parse(buf[:bufLen])
+		if err != nil {
+			return nil, err
+		}
+
+		copy(buf, buf[readN:bufLen])
+
+		bufLen -= readN
 	}
-	str := string(data)
-	requestLine, _, err := parseRequestLine(str)
-	if err != nil {
-		return nil, err
-	}
-	request := Request{*requestLine}
-	return &request, nil
+
+	return request, nil
 }
 
-func parseRequestLine(data string) (*RequestLine, string, error) {
-	idx := strings.Index(data, SEPARATOR)
+func parseRequestLine(data []byte) (*RequestLine, int, error) {
+	idx := bytes.Index(data, SEPARATOR)
+	parsedBytes := 0
 	if idx == -1 {
-		return nil, "", ErrParsingMessage
+		return nil, parsedBytes, nil
 	}
 	requestLine := data[:idx]
-	restOfMessage := data[idx+len(SEPARATOR):]
+	parsedBytes = idx + len(SEPARATOR)
 
-	parts := strings.Split(requestLine, " ")
+	parts := bytes.Split(requestLine, []byte(" "))
 	if len(parts) != 3 {
-		return nil, restOfMessage, ErrParsingMessage
+		return nil, 0, ErrParsingMessage
 	}
 
 	method := parts[0]
 	requestTarget := parts[1]
 	protocol := parts[2]
 
-	if !slices.Contains(methods, method) {
-		return nil, restOfMessage, ErrMethod
+	if !containByteSlice(methods, method) {
+		return nil, 0, ErrMethod
 	}
 
-	protocolParts := strings.Split(protocol, "/")
-	if len(protocolParts) != 2 || protocolParts[0] != "HTTP" || protocolParts[1] != "1.1" {
-		return nil, restOfMessage, ErrHttpVersion
+	protocolParts := bytes.Split(protocol, []byte("/"))
+	if len(protocolParts) != 2 || !bytes.Equal(protocolParts[0], []byte("HTTP")) || !bytes.Equal(protocolParts[1], []byte("1.1")) {
+		return nil, parsedBytes, ErrHttpVersion
 	}
 	httpVersion := protocolParts[1]
 
-	rl := RequestLine{httpVersion, requestTarget, method}
+	rl := RequestLine{string(httpVersion), string(requestTarget), string(method)}
 
-	return &rl, restOfMessage, nil
+	return &rl, parsedBytes, nil
 
+}
+
+func containByteSlice(slices [][]byte, target []byte) bool {
+
+	for _, slice := range slices {
+		if bytes.Equal(slice, target) {
+			return true
+		}
+	}
+
+	return false
 }
