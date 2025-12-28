230b2d7b249654994b9b09060cb643ddf01d1d33
Rustam Shamsutdinov <sypooit@gmail.com>
2025-12-28 22:03:06 +0300

    use custom request

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strings"
+
+	"httpfromtcp/internal/requests"
 )
 
 func main() {
@@ -21,46 +20,9 @@ func main() {
 			log.Fatal(err)
 		}
 		fmt.Println("Connection is accepted")
-		linesChan := getLinesChannel(connection)
-		for line := range linesChan {
-			fmt.Println(line)
-		}
+		r, err := requests.RequestFromReader(connection)
+		fmt.Printf("Request line:\n- Method: %s\n- Target: %s\n- Version: %s\n", r.RequestLine.Method, r.RequestLine.RequestTarget, r.RequestLine.HttpVersion)
 		fmt.Println("Connection is closed")
 	}
 
-}
-
-func getLinesChannel(stream io.ReadCloser) <-chan string {
-	lines := make(chan string)
-
-	go func() {
-		defer stream.Close()
-		defer close(lines)
-		currentLineContents := ""
-		for {
-			buffer := make([]byte, 8, 8)
-			n, err := stream.Read(buffer)
-			if err != nil {
-				if currentLineContents != "" {
-					lines <- currentLineContents
-					currentLineContents = ""
-				}
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				fmt.Printf("error: %s\n", err.Error())
-				break
-			}
-			str := string(buffer[:n])
-			parts := strings.Split(str, "\n")
-			for i := 0; i < len(parts)-1; i++ {
-				currentLineContents += parts[i]
-				lines <- currentLineContents
-				currentLineContents = ""
-			}
-			currentLineContents += parts[len(parts)-1]
-
-		}
-	}()
-	return lines
 }
